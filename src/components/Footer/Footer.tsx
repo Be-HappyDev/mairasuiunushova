3fc40c030a1352b998ec621b77238bca671c6377
BeUniqueCpp <be.uniquecpp@gmail.com>
2024-01-22 03:04:54 +0600

    change footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,20 @@
+'use client'
+
+import { useChangeLang } from "@/store/store";
+
 export const Footer = () => {
+  const lang = useChangeLang((state) => state.lang)
+
   return (
-    <div style={{textAlign: "center", padding: "20px", borderTop: "1px solid #000", width: "100%"}}>2020 Майра Суйунушова</div>
-  )
-}
+    <div
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        borderTop: "1px solid #000",
+        width: "100%",
+      }}
+    >
+      2020 {lang === "en" ? "Maira Suiunushova" : "Майра Суйунушова"}
+    </div>
+  );
+};
